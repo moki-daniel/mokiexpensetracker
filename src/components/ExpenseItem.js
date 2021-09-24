db52225d57f026d111e27c39a406b036ec6cf64e
moki-daniel <mokidnl@gmail.com>
2021-09-24 14:14:58 +0300

    ft(ExpenseItem)implemented props for data passing & component reuse

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 9, 24);
-  const expenseTitle = "House Rent";
-  const expenseAmount = 100;
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div className="expense-item__date">{expenseDate.toISOString()}</div>
+      <div className="expense-item__date">{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
