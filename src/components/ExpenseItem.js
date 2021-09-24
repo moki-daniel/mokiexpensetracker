5d7f3f311741098f0d4c5a4c8cef90bd5619c230
moki-daniel <mokidnl@gmail.com>
2021-09-24 18:17:40 +0300

    ft(Card.js) implemented props children in the container

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
-      <div className="expense-item__date">{props.date.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
